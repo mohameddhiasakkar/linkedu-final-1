f0b2a12d74a0f41d003a5eb19a67e3dfe3261ef8
Dhia Sakkar <dhiasakkar@gmail.com>
2026-03-24 21:30:14 +0100

    Update main application bootstrap to use new App component

--- froent/src/main.ts
+++ froent/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app/app.config';
-import { AppComponent } from './app/app/app.component';
+import { App } from './app/app/app';
 import 'zone.js';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
